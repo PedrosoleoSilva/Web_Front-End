a54f4159ed038197b0f6829eaee4f5c76fd36995
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2023-06-07 22:43:28 -0300

    front

---
+++ primeiro-projeto/src/components/Card.js
@@ -0,0 +1,11 @@
+
+import'./Card.css'
+const Card = () => {
+    return (
+        <div className='cardContainer'>
+            <p>Meu primeiro componente em React</p>
+        </div>
+    );
+}
+
+export default Card;
